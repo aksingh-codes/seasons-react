bd0fc94061dc8ae2466dedc9883ea500fb844aa9
aksingh47 <56232421+aksingh47@users.noreply.github.com>
2021-08-30 15:28:55 +0530

    refactor to class component, added state system

---
+++ src/SeasonsDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SeasonsDisplay() {
+    return (
+        <div>
+            hola
+        </div>
+    )
+}
+
+export default SeasonsDisplay
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>hi there!</div>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setstate
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>Latitude: {this.state.lat ? this.state.lat : "loading..."}</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
